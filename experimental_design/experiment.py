aad80af0dd491059814f0dad7dae78b3ea0b7469
F. Robin <frederique.robin21@gmail.com>
2022-10-27 15:08:59 +0200

    basic proposal

--- experimental_design/experiment.py
+++ experimental_design/experiment.py
@@ -1,6 +1,21 @@
-class Experiment:
+from abc import ABC, abstractmethod
+from typing import Dict
+import numpy as np
+
+class AbstractExperiment(ABC):
     def __init__(self, conf) -> None:
+        self.seed = conf.get('seed', )
+        self.sample_size = conf.get('sample_size')
+        self.name = conf.get('name')
         pass
 
-    def generate(self) :
+    def get_name(self):
+        return self.name 
+
+    def generator(self) :
+        for _ in range(self.sample_size):
+            yield self.draw()
+
+    @abstractmethod
+    def draw(self) -> np.array:
         pass
