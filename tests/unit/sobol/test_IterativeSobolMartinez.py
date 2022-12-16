6c15f2575f84aa1d94bdf3315dbd8138803ddbeb
Frobinou <34059155+Frobinou@users.noreply.github.com>
2022-12-16 12:39:40 +0100

    Update test_IterativeSobolMartinez.py

--- tests/unit/sobol/test_IterativeSobolMartinez.py
+++ tests/unit/sobol/test_IterativeSobolMartinez.py
@@ -4,7 +4,8 @@ import copy
 
 import numpy as np
 import openturns as ot
-from src.sobol.sobol_martinez import IterativeSobol
+from iterative_stats.sobol.sobol_martinez import IterativeSobol
+from experimental_design.experiment import AbstractExperiment
 
 import logging
 logging.getLogger('matplotlib').setLevel(logging.WARNING)
@@ -14,9 +15,33 @@ logger = logging.getLogger()
 # TODO: 
 #   - Tester avec les fonctions ishigami
 #   - Tester avec OpenTurns
-#
-#            
-       
+
+class PickFreeze(AbstractExperiment):
+    def __init__(self, conf) -> None:  
+        nb_parms = conf.get('nb_parms')
+        self.numpy_sample_C = [np.zeros(self.sample_size) for _ in range(nb_parms)]
+        self.numpy_sample_B = np.zeros(self.sample_size)  
+        distribution = ot.ComposedDistribution([ot.Uniform(-1.0, 1.0)] * 3) 
+        self.inputDesign = ot.SobolIndicesExperiment(distribution, self.sample_size).generate() 
+        formula = [conf.get('formula')]
+        self.model = ot.SymbolicFunction(['X1', 'X2', 'X3'], formula)
+        self.cpt = 0
+
+    def draw(self) :
+        
+        # Apply the pick-freeze approach
+
+        sample_A = self.inputDesign[2*self.cpt]
+        sample_B = self.inputDesign[2*self.cpt + 1]
+        y_C = []
+        for k in range(self.nb_parms):
+            sample_Ck = copy.deepcopy(sample_A)  
+            sample_Ck[k] = sample_B[k]
+            y_C.append(self.model(sample_Ck)[0])
+            self.numpy_sample_C[k][self.cpt] = self.model(sample_Ck)[0]
+        self.numpy_sample_B[self.cpt] = self.model(sample_B)[0]
+        return 
+
 class TestIterativeCovariance(unittest.TestCase):
 
 
@@ -29,55 +54,46 @@ class TestIterativeCovariance(unittest.TestCase):
         # Draw samples A and B (here A = (X1, X2, X3), where X1, X2 et X3 are iid and follows a Unif[a,b])
         nb_parms = 3
         sobol = IterativeSobol({'vector_size' :1, 'nb_parms':nb_parms})
-        nb_sim = 10000
+        nb_sim = 3
 
         # Create the model and input distribution
-        a, b, c = 1, 7 , 0.1
-        formula = [f'{a}*sin(pi_*X1)+{b}*sin(X2)*sin(pi_*X2)+' + \
-           f'{c}*((pi_*X3)^4)*sin(pi_*X1)']
-        input_names = ['X1', 'X2', 'X3']
-        model = ot.SymbolicFunction(input_names, formula)
-        distribution = ot.ComposedDistribution([ot.Uniform(-1.0, 1.0)] * 3, \
-                                        ot.IndependentCopula(3))
+        formula = ['sin(pi_*X1)+7*sin(pi_*X2)^2+0.1*(pi_*X3)^4*sin(pi_*X1)']
+        model = ot.SymbolicFunction(['X1', 'X2', 'X3'], formula)
+        distribution = ot.ComposedDistribution([ot.Uniform(-1.0, 1.0)] * 3)
         dimension = distribution.getDimension()
         ot.RandomGenerator.SetSeed(0)
-        inputDesign = ot.SobolIndicesExperiment(distribution, 2*nb_sim, True).generate()
+        inputDesign = ot.SobolIndicesExperiment(distribution, nb_sim).generate()
         outputDesign = model(inputDesign)
+        logger.info(f'input size : {inputDesign}')
 
-        # with numpy
+        # Check the iterative algorithm
+        # -- Apply the pick-freeze approach
         numpy_sample_C = [np.zeros(nb_sim) for _ in range(nb_parms)]
         numpy_sample_B = np.zeros(nb_sim)
-        # Apply the pick-freeze approach
         for i in range(nb_sim):
-            sample_A = inputDesign[2*i]
-            sample_B = inputDesign[2*i + 1]
+            sample_A = inputDesign[i]
+            sample_B = inputDesign[nb_sim + i]
             
             y_C = []
             for k in range(nb_parms):
                 sample_Ck = copy.deepcopy(sample_A)  
                 sample_Ck[k] = sample_B[k]
+                logger.info(f'sample CK: {sample_Ck}')
                 y_C.append(model(sample_Ck)[0])
-                
-                numpy_sample_C[k][i] = model(sample_Ck)[0]
             
-            numpy_sample_B[i] = model(sample_B)[0]
-                
-            sobol.increment(np.array(outputDesign[2*i+1]), y_C)
-        
+            sobol.increment(np.array(outputDesign[i]), y_C)
         logger.info(f'Sobol iterative: {sobol.get_stats()}')
 
+        # logger.info(f'Sobol numpy ------------')
+        # np_sobol = np.zeros(nb_parms)
+        # for k in range(nb_parms):
+        #     prod_var = np.var(numpy_sample_B,ddof=1)*np.var(numpy_sample_C[k],ddof=1)
+        #     np_sobol[k] = np.cov(numpy_sample_B, numpy_sample_C[k])[0][1]/np.sqrt(prod_var)
+        # logger.info(f'Sobol numpy: {np_sobol}')
 
-        logger.info(f'Sobol numpy ------------')
-        #logger.info(f'Sobol numpy_sample_C: sample={numpy_sample_C[0]}')
-        # logger.info(f'Sobol numpy_sample_B: sample={numpy_sample_B}')
-        logger.info(f'Sobol type: {type(numpy_sample_B)} -- {type(numpy_sample_C[0])}')
-        np_sobol = np.zeros(nb_parms)
-        for k in range(nb_parms):
-            prod_var = np.var(numpy_sample_B,ddof=1)*np.var(numpy_sample_C[k],ddof=1)
-            np_sobol[k] = np.cov(numpy_sample_B, numpy_sample_C[k])[0][1]/np.sqrt(prod_var)
-        logger.info(f'Sobol numpy: {np_sobol}')
-
-        # Compute first order indices using the Saltelli estimator
-        sensitivityAnalysis = ot.SaltelliSensitivityAlgorithm(inputDesign, outputDesign, 2*nb_sim)
+        # Compute first order indices using the Martinez estimator
+        sensitivityAnalysis = ot.MartinezSensitivityAlgorithm(inputDesign, outputDesign, nb_sim)
         first_order = sensitivityAnalysis.getFirstOrderIndices()
-        logger.info(f'Sobol openturns: {first_order}')
+        logger.info(f'Sobol openturns: first order {first_order}')
+        total_order = sensitivityAnalysis.getTotalOrderIndices()
+        logger.info(f'Sobol openturns: total order {total_order}')
