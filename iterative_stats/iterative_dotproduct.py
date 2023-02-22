84ba9d4bebfa99f1a9dc43d81d6e875fbf53003e
Frédérique ROBIN <frederique.robin21@gmail.com>
2023-02-22 11:05:12 +0100

    add dot product

--- iterative_stats/iterative_dotproduct.py
+++ iterative_stats/iterative_dotproduct.py
@@ -3,14 +3,41 @@ import numpy as np
 import copy
 
 from iterative_stats.abstract_iterative_statistics import AbstractIterativeStatistics
-from iterative_stats.iterative_mean import IterativeMean
+from iterative_stats.iterative_mean import IterativeShiftedMean
 from iterative_stats.utils.logger import logger 
 
 class IterativeDotProduct(AbstractIterativeStatistics):
     def __init__(self, vector_size : int = 1):
         super().__init__(vector_size)
 
-    def increment(self, data_1, data_2):
+        self.iterative_shifted_mean_1 = IterativeShiftedMean(vector_size)
+        self.iterative_shifted_mean_2 = IterativeShiftedMean(vector_size)
+
+        self.previous_shift = None
+        self.collect_data = {'data_1' : [], 'data_2' : []}
+
+    def increment(self, data_1, data_2, shift):
         self.iteration += 1
-        self.state += data_1 * data_2
         
+        if self.iteration == 1 :
+            self.collect_data['data_1'].append(data_1)
+            self.collect_data['data_2'].append(data_2)
+
+        elif self.iteration == 2 :
+            self.collect_data['data_1'].append(data_1)
+            self.collect_data['data_2'].append(data_2)
+
+            self.state = np.dot(self.collect_data['data_1'] - shift, self.collect_data['data_2'] - shift)
+            del self.collect_data
+        else :
+            
+            diff_shift = self.previous_shift - shift
+            self.state *= (1 - 1/(self.iteration - 1))
+            self.state += (data_1 - self.previous_shift)*(data_2 - self.previous_shift)/(self.iteration - 1)
+            val = self.iterative_shifted_mean_1.get_stats() + self.iterative_shifted_mean_2.get_stats()
+            val +=  diff_shift + (data_1 + data_2 - shift - self.previous_shift)/(self.iteration - 1)
+            self.state += val * diff_shift
+        # Update
+        self.previous_shift = shift
+        self.iterative_shifted_mean_1.increment(data_1, shift)
+        self.iterative_shifted_mean_2.increment(data_2, shift)
