28e0cf6ae6b051ad27f7401a591a3759f8147c42
Abhishek Purandare <abhishek.purandare@inria.fr>
2025-02-05 16:19:42 +0100

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="iterative_stats",
-    version="0.1.0",
+    version="0.1.1",
     description="This package implements iterative algorithms to compute some basics statistics",
     author="Frederique Robin",
     license="BSD License",
